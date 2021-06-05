b2dba76324f0b6dee48939f56d8c26aba051f6bc
Gaia Liistro <gaia@summy.global>
2021-06-05 15:45:08 +0200

    first commit:

--- experiments/aggregation/aggregation.py
+++ experiments/aggregation/aggregation.py
@@ -1,3 +1,4 @@
+from experiments import aggregation
 from experiments.aggregation.cockroach import Cockroach
 from experiments.aggregation.config import config
 from simulation.utils import *
@@ -5,5 +6,59 @@ from simulation.swarm import Swarm
 
 
 class Aggregations(Swarm):
-    """ """
-    pass
+    """
+    This class inherits from the base class Swarm.
+
+    """
+    def __init__(self, screen_size) -> None:
+        """
+        This function is the initializer of the class Aggregation.
+        :param screen_size:
+        """
+        super(Aggregations, self).__init__(screen_size)
+        self.object_loc = config["aggregation"]["site"]
+
+    def initialize(self, num_agents: int) -> None:
+        """
+        Initialize the whole swarm, creating and adding the aggregation areas, and the agent, placing them inside of the
+        screen.
+        :param num_agents: int:
+"""
+    #add sites
+        if config['aggregation']['site']:
+            object_loc=config['aggregation']['location']
+            filename = ("experiments/aggregation/images/greyc1.png")
+            self.objects.add_object(
+                file=filename, pos=object_loc, scale=config['aggregation']['scale'], obj_type="site")
+        if config['aggregation']['site2']:
+            object_loc=config['aggregation']['location2']
+            filename = ("experiments/aggregation/images/greyc1.png")
+            self.objects.add_object(
+                file=filename, pos=object_loc, scale=config['aggregation']['scale2'], obj_type="site")
+    #add outside borders
+        if config['aggregation']['outside']:
+            scale = [800, 800]
+            filename = ("experiments/flocking/images/convex.png")
+#("experiments/aggregation/images/greyc2.png")
+            object_loc = config["base"]["object_location"]
+            self.objects.add_object(
+                file=filename, pos=object_loc, scale=scale, obj_type="obstacle"
+            )
+            min_x, max_x = area(object_loc[0], scale[0])
+            min_y, max_y = area(object_loc[1], scale[1])
+    #add agents
+        for index, agent in enumerate(range(num_agents)):
+            coordinates = generate_coordinates(self.screen)
+            if config['aggregation']['outside']:
+                while (
+                        coordinates[0] >= max_x
+                        or coordinates[0] <= min_x
+                        or coordinates[1] >= max_y
+                        or coordinates[1] <= min_y
+                    ):
+                        coordinates = generate_coordinates(self.screen)
+
+
+            self.add_agent(Cockroach(pos=np.array(coordinates), v=None, aggregation=self, index=index))
+
+
--- experiments/aggregation/cockroach.py
+++ experiments/aggregation/cockroach.py
@@ -1,8 +1,99 @@
 from experiments.aggregation.config import config
 from simulation.agent import Agent
 from simulation.utils import *
+import random
 
 
 class Cockroach(Agent):
     """ """
-    pass
+    def __init__(
+            self, pos, v, aggregation, index: int, image: str = "experiments/aggregation/images/ant.png"
+    ) -> None:
+        """
+        Args:
+        ----
+            pos:
+            v:
+            flock:
+            index (int):
+            image (str): Defaults to "experiments/aggregation/images/ant.png"
+        """
+        super(Cockroach, self).__init__(
+            pos,
+            v,
+            image,
+            max_speed=config["agent"]["max_speed"],
+            min_speed=config["agent"]["min_speed"],
+            mass=config["agent"]["mass"],
+            width=config["agent"]["width"],
+            height=config["agent"]["height"],
+            dT=config["agent"]["dt"],
+            index=index
+        )
+
+        self.aggregation = aggregation
+        # self.still = None us it like thsi 
+
+    def update_actions(self) -> None:
+        # find all the neighbors of a boid based on its radius view
+        neighbors = self.aggregation.find_neighbors(self, config["cockroach"]["radius_view"])
+         #get the amount of neighbors
+        n_neighbors = len(neighbors)
+        #define variables TO DO config.toml
+        wander_dist = 100
+        wander_radius = 100
+        wander_angle = 100
+        leave = True
+        wander = True
+        still = False
+
+         # avoid any obstacles in the environment
+        for obstacle in self.aggregation.objects.obstacles:
+            collide = pygame.sprite.collide_mask(self, obstacle)
+            if bool(collide):
+                self.avoid_obstacle()
+
+
+        #change between states
+        #define wandering state, not sure if it is correct
+        for site in self.aggregation.objects.sites:
+            collide = pygame.sprite.collide_mask(self, site)
+            #if not in the site or not in the leave state then wander
+            if not bool(collide) and leave == True:
+                self.wander(wander_dist, wander_radius, wander_angle)
+            #if in the site and not in the leave state then join
+            elif bool(collide) and leave == True: 
+                join()
+            
+
+        #     if bool(collide):
+        #         self.join(n_neighbors)
+        #         print("site")
+        #     else:
+        #         self.wander(wander_dist, wander_radius, wander_angle)
+        #         print('wander')
+    # Joining (decided to join an aggregation)
+
+    def join(self,n_neighbour): 
+        global still
+        #the probaility of joining is queal to the amount of neighbours divided by the total population
+        prob = n_neighbour/config['base']['n_agents']
+        sample = random.uniform(0,1)
+        if sample < prob:
+            #wait T join
+            still = True
+            # return True 
+    # Still (stop in the aggregate location)
+
+    def still(self, n_neighbours):
+        global still
+        prob = n_neighbour/config['base']['n_agents']
+        sample = random.uniform(0,1)
+        if sample < prob:
+            still = False
+    # Leaving (where the agent decided to start Wandering
+
+    def leave(self):
+        #start walking
+        #wait T leaves
+        return True
